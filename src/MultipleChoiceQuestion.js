0f40f03abdcc35ffa062b535d4283e35a1a50f96
PaulJKathmann <paul.kathmann.98@gmail.com>
2023-08-11 17:31:00 +0200

    text left aligned

--- src/MultipleChoiceQuestion.js
+++ src/MultipleChoiceQuestion.js
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
-import {ReactComponent as IconWrinkles} from "./assets/icons/Wrinkles.svg";
-import {ReactComponent as IconThinLips} from "./assets/icons/ThinLips.svg";
-import {ReactComponent as IconFacialVolumising} from "./assets/icons/FacialVolumising.svg";
-import {ReactComponent as IconUnevenSkinTone} from "./assets/icons/UnevenSkinTone.svg";
-import {ReactComponent as IconAcne} from "./assets/icons/Acne.svg";
-import {ReactComponent as IconDoubleChin} from "./assets/icons/DoubleChin.svg";
-import {ReactComponent as IconDarkCircles} from "./assets/icons/DarkCircles.svg";
-import {ReactComponent as IconDrySkin} from "./assets/icons/DrySkin.svg";
-import {ReactComponent as IconOilySkin} from "./assets/icons/OilySkin.svg";
+import React, { useState } from "react";
+import { ReactComponent as IconWrinkles } from "./assets/icons/Wrinkles.svg";
+import { ReactComponent as IconThinLips } from "./assets/icons/ThinLips.svg";
+import { ReactComponent as IconFacialVolumising } from "./assets/icons/FacialVolumising.svg";
+import { ReactComponent as IconUnevenSkinTone } from "./assets/icons/UnevenSkinTone.svg";
+import { ReactComponent as IconAcne } from "./assets/icons/Acne.svg";
+import { ReactComponent as IconDoubleChin } from "./assets/icons/DoubleChin.svg";
+import { ReactComponent as IconDarkCircles } from "./assets/icons/DarkCircles.svg";
+import { ReactComponent as IconDrySkin } from "./assets/icons/DrySkin.svg";
+import { ReactComponent as IconOilySkin } from "./assets/icons/OilySkin.svg";
 
 
 
 
 
 
-function MultipleChoiceQuestion({ question, options, setAnswer, setError}) {
+function MultipleChoiceQuestion({ question, options, setAnswer, setError }) {
     const optionIcons = {
         'Wrinkles': IconWrinkles,
         'Thin lips': IconThinLips,
@@ -42,25 +42,25 @@ function MultipleChoiceQuestion({ question, options, setAnswer, setError}) {
             {options.map((option, index) => {
                 const Icon = optionIcons[option];
                 return (
-                <div key={index} className={`${
-                    question.answer.includes(option)
-                      ? "choice-selected"
-                      : "border-gray-200 border custom-text-color"
-                  } h-16 p-2 rounded bg-white cursor-pointer transition-all flex flex-col items-center justify-center`}
-                  onClick={() => handleOptionsChange(option)}>
-                    <input  
-                        type="checkbox"
-                        id={`option-${index}`}
-                        name={`multiple-selection-${question.id}`}
-                        value={option}
-                        className="hidden"
-                        checked={question.answer.includes(option)}
-                        readOnly
-                    />
-                    <label htmlFor={`option-${index}`} onClick={(e) => e.stopPropagation()} className="text-sm flex items-center my-2 cursor-pointer font-custom">
-                    {option}
-                    </label>
-                </div>
+                    <div key={index} className={`${
+                        question.answer.includes(option)
+                            ? "choice-selected"
+                            : "border-gray-200 border custom-text-color"
+                        } h-16 p-2 rounded bg-white cursor-pointer transition-all flex flex-col text-left`}
+                        onClick={() => handleOptionsChange(option)}>
+                        <input
+                            type="checkbox"
+                            id={`option-${index}`}
+                            name={`multiple-selection-${question.id}`}
+                            value={option}
+                            className="hidden"
+                            checked={question.answer.includes(option)}
+                            readOnly
+                        />
+                        <label htmlFor={`option-${index}`} onClick={(e) => e.stopPropagation()} className="text-xs text-left flex items-center my-2 cursor-pointer font-custom">
+                            {option}
+                        </label>
+                    </div>
                 )
             })}
         </div>
